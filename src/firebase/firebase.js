8a5d540745ebd49d0403ae23d2a97801bf5f0e02
Hadi <mh.elhadi@outlook.com>
2020-08-12 23:08:19 -0400

    git add env variable to heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,7 @@ import "firebase/database";
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: "https://expense-moi.firebaseio.com",
+  databaseURL: process.env.REACT_APP_DATABASEURL,
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
